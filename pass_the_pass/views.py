1d91291de6a30d64216e7f4922d56c861d51722a
ALFomas <13fominal@gmail.com>
2024-07-05 00:08:10 +0300

    created views for summbit data API

--- pass_the_pass/views.py
+++ pass_the_pass/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Camping
+from .serializers import CampingSerializer
 
-# Create your views here.
+
+class SubmitData(APIView):
+    """Сlass that processes POST type requests"""
+
+    def post(self, request):
+        """Method for post data"""
+        data = request.data
+        camping_serializer = CampingSerializer(data=data)
+
+        if camping_serializer.is_valid():
+            camping = camping_serializer.save()
+            camping.submit_to_db()
+
+            return Response(camping_serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(camping_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
