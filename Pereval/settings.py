7763b43d1e6613cc01023405ecd412113dcb0dc9
ALFomas <13fominal@gmail.com>
2024-07-04 23:02:39 +0300

    created new app pass_the_pass

--- Pereval/settings.py
+++ Pereval/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pass_the_pass'
 ]
 
 MIDDLEWARE = [
---
+++ pass_the_pass/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PassThePassConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'pass_the_pass'
